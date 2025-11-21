926fdfdca965d8db07de0caaa7bffea72f01db4f
Daniel Grimes <dan@dcgnet.co.uk>
2025-11-21 15:04:20 +0000

    install script and workspace testing

---
+++ files/scripts/install-powershell.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# Install PowerShell on Ubuntu
+
+# Update package list
+apt-get update
+
+# Install dependencies
+apt-get install -y wget apt-transport-https software-properties-common
+
+# Get Ubuntu version
+source /etc/os-release
+
+# Download the Microsoft repository keys
+wget -q "https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb"
+
+# Register the Microsoft repository keys
+dpkg -i packages-microsoft-prod.deb
+
+# Delete the Microsoft repository keys file
+rm packages-microsoft-prod.deb
+
+# Update package list
+apt-get update
+
+# Install PowerShell
+apt-get install -y powershell
