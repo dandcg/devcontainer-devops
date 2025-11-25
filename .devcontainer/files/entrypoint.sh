2705af1ba792a6bd615f371252190c0764ef7d49
Daniel Grimes <daniel.grimes@i3works.co.uk>
2025-11-25 18:43:02 +0000

    updates to docker hostnetworking

--- .devcontainer/files/entrypoint.sh
+++ .devcontainer/files/entrypoint.sh
@@ -9,4 +9,10 @@ if [ ! -e /home/vscode/.initialized ]; then
     chown -R vscode:vscode /home/vscode
 fi
 
+# Fix hostname resolution for sudo
+HOSTNAME=$(hostname)
+if ! grep -q "$HOSTNAME" /etc/hosts; then
+    echo "127.0.0.1 $HOSTNAME" >> /etc/hosts
+fi
+
 exec "$@"
